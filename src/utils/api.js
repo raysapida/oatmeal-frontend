13f97f371c96c0e008740afa9feed22f41d20563
Raymond Sapida <ray.sapida@gmail.com>
2019-03-15 07:54:51 -0700

    Update to use heroku deployed microservice instead of local

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 var api = axios.create({
-  baseURL: "http://localhost:3000/api"
+  baseURL: "https://tweets-service.herokuapp.com/api"
 });
 
 export const getAllTweets = async (searchTerm) => {
